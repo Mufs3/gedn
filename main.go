d89d9aa61d5febbcc28afab152c85b95488eabf5
Agustin_Minuz <avminuz@localhost.localdomain>
2023-12-23 15:46:20 -0300

    Finished basic functionality

--- main.go
+++ main.go
@@ -7,6 +7,35 @@ import (
 
 const Port = "8080"
 
+type TMessage struct {
+	Sender  net.Addr
+	Message string
+}
+
+func write2Conn(conn net.Conn, str string) {
+	mess := []byte(str)
+	n, err := conn.Write(mess)
+	if err != nil {
+		log.Printf("Error to writing to conn: %s", err)
+	}
+	for n < len(mess) {
+		auxn, err := conn.Write(mess[n:])
+		n = auxn + n
+		if err != nil {
+			log.Printf("Error to writing to conn: %s", err)
+		}
+	}
+}
+
+func readFromConn(conn net.Conn) string {
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Cant read from conn by error: %s", err)
+	}
+	return string(buffer)
+}
+
 func init_client() net.Listener {
 	listener, err := net.Listen("tcp", ":"+Port)
 	if err != nil {
@@ -15,36 +44,64 @@ func init_client() net.Listener {
 	return listener
 }
 
-func init_conn(conn net.Conn, connected_users map[net.Addr]string) {
-	var buffer [1024]byte
+func init_conn(conn net.Conn, connected_user map[net.Addr]string) {
+	var buffer [64]byte
 
-	mess := []byte("Welcome, please provide a Name:\t")
-	_, err := conn.Write(mess)
+	write2Conn(conn, "Welcome, please provide a Name:\t")
+
+	_, err := conn.Read(buffer[:])
 	if err != nil {
-		log.Printf("Error on writing to conn: %s\n", err)
+		log.Printf("Error on reading conn: %s\n", err)
 	}
-	_, err = conn.Read(buffer[:])
-	if err != nil {
-		log.Printf("Error on writing to conn: %s\n", err)
-	}
-	connected_users[conn.RemoteAddr()] = string(buffer[:])
+	connected_user[conn.RemoteAddr()] = string(buffer[:])
 }
 
-func handle_conn(conn net.Conn, connected_users map[net.Addr]string) {
+func writeMessage(conn net.Conn, connected_users map[net.Addr]string, output chan TMessage) {
+	for {
+		str := readFromConn(conn)
+		output <- TMessage{conn.RemoteAddr(), str}
+	}
+}
+
+func readMessage(conn net.Conn, connected_users map[net.Addr]string, input chan TMessage) {
+	for {
+		message := <-input
+		if message.Sender != conn.RemoteAddr() {
+			write2Conn(conn, formatMessage(message, connected_users))
+		}
+	}
+}
+
+func formatMessage(message TMessage, connected_users map[net.Addr]string) string {
+	user_name := connected_users[message.Sender]
+	str := user_name + ">>\t" + message.Message
+	return str
+}
+
+func handle_conn(conn net.Conn, connected_users map[net.Addr]string, messages chan TMessage) {
 	init_conn(conn, connected_users)
+	defer conn.Close()
+
 	user_name := connected_users[conn.RemoteAddr()]
-	conn.Write([]byte("Hi " + user_name + "gt fckd\n"))
-	conn.Close()
+
+	write2Conn(conn, "Hi "+user_name)
+
+	go writeMessage(conn, connected_users, messages)
+	go readMessage(conn, connected_users, messages)
+
+	recover()
+	return
 }
 
 func main() {
 	listener := init_client()
 	connected_users := make(map[net.Addr]string)
+	messages := make(chan TMessage)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("Can't accept connection in port %s by error: %s\n", Port, err)
 		}
-		go handle_conn(conn, connected_users)
+		go handle_conn(conn, connected_users, messages)
 	}
 }
